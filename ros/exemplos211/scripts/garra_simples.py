8258a8e1b93c0add855639565958192ef9fc4a9a
mirwox <fmiranda@gmail.com>
2021-04-29 12:37:03 -0700

    exemplo da garra comentado

--- ros/exemplos211/scripts/garra_simples.py
+++ ros/exemplos211/scripts/garra_simples.py
@@ -12,12 +12,13 @@ if __name__ == "__main__":
 
     try:
         while not rospy.is_shutdown():
-            ombro.publish(-1.0)
-            garra.publish(0.0)            
+            ombro.publish(-1.0) ## para baixo
+            garra.publish(0.0)  ## Fechado
             rospy.sleep(3.0)
-            ombro.publish(1.5)
-            garra.publish(-1.0) 
+            ombro.publish(1.5) ## para cima
+            garra.publish(-1.0) ## Aberto
             rospy.sleep(3.0)
+            ombro.publish(0.0) ## para frente
 
     except rospy.ROSInterruptException:
         print("Ocorreu uma exceção com o rospy")
