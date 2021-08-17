4f8ddc1be42bde3ef51ea64cb0e83a3d3f81f6ab
DiegoSoler <dpsoler09@gmail.com>
2021-08-17 17:13:31 -0300

    Update webcam.py

--- aula02/webcam.py
+++ aula02/webcam.py
@@ -10,15 +10,15 @@ import fotogrametria
 
 # ->>> !!!! FECHE A JANELA COM A TECLA ESC !!!! <<<<-
 
-def calcular_anguno_e_distancia_na_image_da_webcam(img):
+def calcular_anguno_e_distancia_na_image_da_webcam(img, f):
     """Não mude ou renomeie esta função
         ->>> !!!! FECHE A JANELA COM A TECLA ESC !!!! <<<<-
         deve receber a imagem da camera e retornar uma imagems com os contornos desenhados e os valores da distancia e o angulo.
     """
-    distancia = 0
+    D = 0
     angulo = 0
 
-    return img, distancia, angulo
+    return img, D, angulo
 
 def desenhar_na_image_da_webcam(img, distancia, angulo):
     """Não mude ou renomeie esta função
@@ -31,6 +31,8 @@ def desenhar_na_image_da_webcam(img, distancia, angulo):
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
+## -> Mude o Foco <- ##
+f = fotogrametria.encontrar_foco(1, 1, 1)
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -38,7 +40,7 @@ else:
     rval = False
 
 while rval:
-    img, distancia, angulo = calcular_anguno_e_distancia_na_image_da_webcam(frame)
+    img, distancia, angulo = calcular_anguno_e_distancia_na_image_da_webcam(frame, f)
     img = desenhar_na_image_da_webcam(img, distancia, angulo)
     cv2.imshow("preview", img)
     rval, frame = vc.read()
